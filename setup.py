f697650565ddc042d97b55282e8fb6a3edafbf8c
rbi-international <rohit.bharti8211@gmail.com>
2025-05-09 10:17:07 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'rohit',
+    author_email= 'rohit.bharti8211@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
